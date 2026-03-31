d1753a1730e228cec569721f3e4ed7d7af2b8531
maxcbc <max.bladen-clark@esynergy.com>
2026-03-31 10:02:53 +0100

    add oidc controller tests

--- src/server/api/oidc/controllers/oidc-controller.test.js
+++ src/server/api/oidc/controllers/oidc-controller.test.js
@@ -1,25 +1,51 @@
-import { createServer } from '../../../server.js'
-import { statusCodes } from '../../../common/constants/status-codes.js'
+import { describe, test, expect, vi, afterEach } from 'vitest'
+import { config } from '../../../../config/config.js'
+import { oidcController } from './oidc-controller.js'
 
-describe('#healthController', () => {
-  let server
+const mocks = {
+  configGet: vi.spyOn(config, 'get'),
+  code: vi.fn(),
+  response: vi.fn()
+}
 
-  beforeAll(async () => {
-    server = await createServer()
-    await server.initialize()
+describe('oidcController', () => {
+  afterEach(() => {
+    vi.resetAllMocks()
   })
 
-  afterAll(async () => {
-    await server.stop({ timeout: 0 })
-  })
+  test('it returns the openid configuration discovery document', () => {
+    // Arrange
+    mocks.configGet.mockReturnValue('https://test.example.com')
+    mocks.code.mockReturnThis()
+    mocks.response.mockReturnValue({ code: mocks.code })
+    const h = { response: mocks.response }
 
-  test('Should provide expected response', async () => {
-    const { result, statusCode } = await server.inject({
-      method: 'GET',
-      url: '/health'
-    })
+    // Act
+    oidcController.handler({}, h)
 
-    expect(result).toEqual({ message: 'success' })
-    expect(statusCode).toBe(statusCodes.ok)
+    // Assert
+    const body = mocks.response.mock.calls[0][0]
+    expect(body).toHaveProperty(
+      'authorization_endpoint',
+      'https://test.example.com/authorize'
+    )
+    expect(body).toHaveProperty(
+      'end_session_endpoint',
+      'https://test.example.com/signout'
+    )
+    expect(body).toHaveProperty('jwks_uri', 'https://test.example.com/jwks')
+    expect(body).toHaveProperty(
+      'token_endpoint',
+      'https://test.example.com/token'
+    )
+    expect(body).toHaveProperty(
+      'pushed_authorization_request_endpoint',
+      'https://test.example.com/request'
+    )
+    expect(body).toHaveProperty(
+      'userinfo_endpoint',
+      'https://test.example.com/userinfo'
+    )
+    expect(mocks.code).toHaveBeenCalledWith(200)
   })
 })
